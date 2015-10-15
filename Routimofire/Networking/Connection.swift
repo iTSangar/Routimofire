8d75373dbaa45c90bb6e96df070a20b1920ce74f
iTSangar <itsangardev@gmail.com>
2015-10-15 13:46:19 -0300

    re-indent

--- Routimofire/Networking/Connection.swift
+++ Routimofire/Networking/Connection.swift
@@ -12,39 +12,42 @@ import Alamofire
 class ConnectionUser {
     
     class func createUserWith(params params: [String : AnyObject]) {
-        Alamofire.request(RouterUser.CreateUser(params)).responseJSON(completionHandler: {
-            response in
-            switch response.result {
-            case .Success(let JSON):
-                print(JSON)
-            case .Failure(let error):
-                print(error)
-            }
-        })
+        Alamofire.request(RouterUser.CreateUser(params))
+            .responseJSON(completionHandler: {
+                response in
+                switch response.result {
+                case .Success(let JSON):
+                    print(JSON)
+                case .Failure(let error):
+                    print(error)
+                }
+            })
     }
     
     class func testUserWithoutParams() {
-        Alamofire.request(RouterUser.TestUser).responseJSON(completionHandler: {
-            response in
-            switch response.result {
-            case .Success(let JSON):
-                print(JSON)
-            case .Failure(let error):
-                print(error)
-            }
-        })
+        Alamofire.request(RouterUser.TestUser)
+            .responseJSON(completionHandler: {
+                response in
+                switch response.result {
+                case .Success(let JSON):
+                    print(JSON)
+                case .Failure(let error):
+                    print(error)
+                }
+            })
     }
     
     class func updateUserWith(name name: String, params: [String : AnyObject]) {
-        Alamofire.request(RouterUser.UpdateUser(name, params)).responseJSON(completionHandler: {
-            response in
-            switch response.result {
-            case .Success(let JSON):
-                print(JSON)
-            case .Failure(let error):
-                print(error)
-            }
-        })
+        Alamofire.request(RouterUser.UpdateUser(name, params))
+            .responseJSON(completionHandler: {
+                response in
+                switch response.result {
+                case .Success(let JSON):
+                    print(JSON)
+                case .Failure(let error):
+                    print(error)
+                }
+            })
     }
     
 }
